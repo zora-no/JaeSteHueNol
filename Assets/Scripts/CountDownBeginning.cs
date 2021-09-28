8857909ada975b1ff5c00eb40ad06b1e61380d71
Timon <tjaeger@uos.com>
2021-09-28 13:46:16 +0200

    added page switch for 3-2-1 pages and that they count down with visuals

--- Assets/Scripts/CountDownBeginning.cs
+++ Assets/Scripts/CountDownBeginning.cs
@@ -4,32 +4,101 @@ using UnityEngine;
 
 public class CountDownBeginning : MonoBehaviour
 {
-    public GameObject onePage;
-    public GameObject twoPage;
-    public GameObject threePage;
+    [SerializeField]
+    private GameObject onePage;
+    [SerializeField]
+    private GameObject twoPage;
+    [SerializeField]
+    private GameObject threePage;
+    [SerializeField]
+    private GameObject goPage;
 
     void Start()
     {
-        onePage.SetActive(false);
-        twoPage.SetActive(false);
-        threePage.SetActive(false);
+        SetPageState(PageState.None);
     }
-    
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("counting inst");
+            ShowCountdown();
+        }
+            
+    }
+    enum PageState
+        // the five page states
+    {
+        None,
+        Three,
+        Two,
+        One,
+        Go
+        
+    }
     
     public void ShowCountdown()
     {
-        onePage.SetActive(true);
-        CountingDown();
-        twoPage.SetActive(true);
-        CountingDown();
-        threePage.SetActive(true);
-        CountingDown();
+        StartCoroutine(CountingDown());
+        Debug.Log("counting down function");
+        //SetPageState(PageState.Three);
     }
+    
     
     IEnumerator CountingDown()
     {
-        
+        Debug.Log("counting down Coroutine");
         yield return new WaitForSeconds(1);
+        SetPageState(PageState.Three);
+        yield return new WaitForSeconds(1);
+        SetPageState(PageState.Two);
+        yield return new WaitForSeconds(1);
+        SetPageState(PageState.One);
+        yield return new WaitForSeconds(1);
+        SetPageState(PageState.Go);
+        yield return new WaitForSeconds(2);
+        SetPageState(PageState.None);
         
+        
+    }
+    
+    void SetPageState(PageState state)
+        // activate needed page and deactivate all others
+    {
+        switch (state)
+        {
+            case PageState.None:
+                threePage.SetActive(false);
+                twoPage.SetActive(false);
+                onePage.SetActive(false);
+                goPage.SetActive(false);
+                break;
+            case PageState.Three:
+                threePage.SetActive(true);
+                twoPage.SetActive(false);
+                onePage.SetActive(false);
+                goPage.SetActive(false);
+                break;
+            case PageState.Two:
+                threePage.SetActive(false);
+                twoPage.SetActive(true);
+                onePage.SetActive(false);
+                goPage.SetActive(false);
+                break;
+            case PageState.One:
+                threePage.SetActive(false);
+                twoPage.SetActive(false);
+                onePage.SetActive(true);
+                goPage.SetActive(false);
+                break;
+            case PageState.Go:
+                threePage.SetActive(false);
+                twoPage.SetActive(false);
+                onePage.SetActive(false);
+                goPage.SetActive(true);
+                break;
+            
+           
+        }
     }
 }
